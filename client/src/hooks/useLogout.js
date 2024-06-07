9ddd0084797d83cb9af809fce41c590009d663fd
Sara Martín Rodríguez <e.smarrod02@go.ugr.es>
2024-06-07 09:22:20 +0200

    logout hook

---
+++ client/src/hooks/useLogout.js
@@ -0,0 +1,19 @@
+import {useAuthContext} from './user/useAuthContext'
+
+export const useLogout = () =>{
+
+    const {dispatch} = useAuthContext();
+
+
+    const logout =()=>{ // we don't have to send a request to the backend
+        // remove user form storage
+
+        localStorage.removeItem('user');
+        
+        // dispatch logout action
+        dispatch ({type:'LOGOUT'});
+
+    }
+
+    return {logout}
+}
