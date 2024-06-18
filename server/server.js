4068a8ec7639c1eb0da7e7e03dffcd6d3697b3f4
Sara Martín Rodríguez <e.smarrod02@go.ugr.es>
2024-06-18 18:49:02 +0200

    try to fix server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
-import cors from "cors";
-import records from "./routes/record.js";
+//import cors from "cors";
+//import records from "./routes/record.js";
 import {userRoutes} from './routes/user.js';
 import 'dotenv/config';
 import mongoose from 'mongoose';
@@ -11,8 +11,12 @@ const app = express();
 //routes
 app.use(cors());
 app.use(express.json());
-app.use("/record", records);
-app.use("/user", userRoutes);
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+//app.use("/record", records);
+app.use("/api/user", userRoutes);
 
 
 
