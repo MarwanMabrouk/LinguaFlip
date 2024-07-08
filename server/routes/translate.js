283e12f961d1dd2ed87412459fd89da16f8b5323
Marwan Mabrouk <marwanmabrouk@gmail.com>
2024-07-08 11:37:39 +0200

    Translate Codes

--- server/routes/translate.js
+++ server/routes/translate.js
@@ -4,10 +4,10 @@ import express from "express";
 import { requireAuth } from "../middleware/requireAuth.js";
 import * as deepl from 'deepl-node';
 
+import {sourceLanguages, targetLanguages} from "../routes/translateMap.js";
 
 export const translateRoutes = express.Router();
 translateRoutes.use(requireAuth); // require auth 
-
 
 const translator = new deepl.Translator(process.env.DEEPL_API_KEY);
 
@@ -23,6 +23,9 @@ async function translateText(req, res) {
         console.log(req.body);
         let sourceLanguage = User.nativeLanguage;
         let targetLanguage = User.foreignLanguage;
+        console.log("sourceLanguage is ", sourceLanguage);
+        console.log("targetLanguage is ", targetLanguage);
+        console.log("Mapping is ", sourceLanguages[sourceLanguage], targetLanguages[targetLanguage]);
         //TODO: convert sourceLanguage and targetLanguage to deepl language codes
         //https://developers.deepl.com/docs/resources/supported-languages#target-languages
         let result = await translator.translateText(newDocument.text, "EN", "DE"); 
