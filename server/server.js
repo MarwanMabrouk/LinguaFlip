e26eeaf28ae890c65f9c85b3d1176a4749b87666
Marwan Mabrouk <marwanmabrouk@gmail.com>
2024-08-13 20:52:38 +0200

    Refactor

--- server/server.js
+++ server/server.js
@@ -1,53 +1,51 @@
 import express from "express";
 import cors from "cors";
-//import records from "./routes/record.js";
 import 'dotenv/config';
 import mongoose from 'mongoose';
-
-import {userRoutes} from './routes/user.js';
-import {translateRoutes} from './routes/translate.js';
-import { cardListsRoutes } from "./routes/cardLists.js";
 import https from 'https';
 import fs from 'fs';
-// const PORT = process.env.PORT || 5050;
+import { userRoutes } from './routes/user.js';
+import { translateRoutes } from './routes/translate.js';
+import { cardListsRoutes } from "./routes/cardLists.js";
+
 const app = express();
 
-//routes
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
-//app.use("/record", records);
+// Routes
 app.use("/api/user", userRoutes);
 app.use("/api/cardLists", cardListsRoutes);
-app.use("/api/services",translateRoutes);
+app.use("/api/services", translateRoutes);
+
+// HTTPS server setup
 let server;
-try{
-  
+try {
   const privateKey = fs.readFileSync(process.env.KEY_PATH, 'utf8');
   const certificate = fs.readFileSync(process.env.CERT_PATH, 'utf8');
   const ca = fs.readFileSync(process.env.CA_PATH, 'utf8');
-  const credentials = { key: privateKey, cert: certificate,ca:ca };
+  const credentials = { key: privateKey, cert: certificate, ca: ca };
   const httpsServer = https.createServer(credentials, app);
   server = httpsServer;
-}catch(error){
-  console.log(error);
-  console.log("Failed to create https server, falling back to http");
+} catch (error) {
+  console.log("Failed to create HTTPS server, falling back to HTTP");
   server = app;
 }
-// connect to db
+
+// Connect to MongoDB
 mongoose.connect(process.env.ATLAS_URI)
-  .then(() =>{
-      // start the Express server
-      server.listen(process.env.PORT,"0.0.0.0", () => {
-      console.log(`Server listening on port ${process.env.PORT}`);
-});
+  .then(() => {
+    // Start the server
+    const port = process.env.PORT || 5050;
+    server.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
   })
-  .catch((error) =>{
+  .catch((error) => {
     console.log(error);
-  })
-
-
+  });
