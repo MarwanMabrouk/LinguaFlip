94e938996f974d18953dba0ef83d85e1e5ac45cd
Marwan Mabrouk <marwanmabrouk@gmail.com>
2024-08-13 20:54:11 +0200

    Refactor

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-import 'dotenv/config';
-import mongoose from 'mongoose';
-import https from 'https';
-import fs from 'fs';
-import { userRoutes } from './routes/user.js';
-import { translateRoutes } from './routes/translate.js';
+import "dotenv/config";
+import mongoose from "mongoose";
+import https from "https";
+import fs from "fs";
+import { userRoutes } from "./routes/user.js";
+import { translateRoutes } from "./routes/translate.js";
 import { cardListsRoutes } from "./routes/cardLists.js";
 
 const app = express();
@@ -26,9 +26,9 @@ app.use("/api/services", translateRoutes);
 // HTTPS server setup
 let server;
 try {
-  const privateKey = fs.readFileSync(process.env.KEY_PATH, 'utf8');
-  const certificate = fs.readFileSync(process.env.CERT_PATH, 'utf8');
-  const ca = fs.readFileSync(process.env.CA_PATH, 'utf8');
+  const privateKey = fs.readFileSync(process.env.KEY_PATH, "utf8");
+  const certificate = fs.readFileSync(process.env.CERT_PATH, "utf8");
+  const ca = fs.readFileSync(process.env.CA_PATH, "utf8");
   const credentials = { key: privateKey, cert: certificate, ca: ca };
   const httpsServer = https.createServer(credentials, app);
   server = httpsServer;
@@ -38,7 +38,8 @@ try {
 }
 
 // Connect to MongoDB
-mongoose.connect(process.env.ATLAS_URI)
+mongoose
+  .connect(process.env.ATLAS_URI)
   .then(() => {
     // Start the server
     const port = process.env.PORT || 5050;
